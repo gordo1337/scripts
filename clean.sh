7240b0d0fd73d5f4f601c04c442e4da36c248989
Gottfrid <69000233+gordo1337@users.noreply.github.com>
2022-01-27 11:55:00 +0100

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -9,4 +9,13 @@ sync; echo 3 > /proc/sys/vm/drop_caches
 zypper clean -a
 #Update
 zypper refresh
+#Moving to the dir
+cd ~/scripts
+#execute permission
+chmod 755 clean.sh
+# Now you may call the script whenever you are required to clear the ram cache.
+# Now set a cron to clear RAM cache every day at 2 am. Open crontab for editing.
+crontab -e
+# Append the below line, save and exit to run it at 2 am daily.
+0  2  *  *  *  ~/scripts/clean.sh
 exit 0
